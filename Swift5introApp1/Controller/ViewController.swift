ff18030e41d79a8793da5ebad4a4cfb8f6a1e781
yukifu027 <63824425+yukifu027@users.noreply.github.com>
2020-05-13 11:10:54 +0900

    init commit

---
+++ Swift5introApp1/Controller/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  Swift5introApp1
+//
+//  Created by 福井祐希 on 2020/05/12.
+//  Copyright © 2020 Yuki Fukui. All rights reserved.
+//
+
+import UIKit
+import Lottie
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    var onboardArray = ["1","2","3","4","5"]
+
+    var onboardStringArray = ["私たちはみんなで繋がっています","Good","Hooo","GoodBye","Thankyou"]
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        scrollView.isPagingEnabled = true
+        setUpScroll()
+
+        for i in 0...4{
+
+            let animationView = AnimationView()
+            let animation = Animation.named(onboardArray[i])
+            animationView.frame = CGRect(x: CGFloat(i) * view.frame.size.width, y: 0, width: view.frame.size.width, height: view.frame.size.height)
+            animationView.animation = animation
+            animationView.contentMode = .scaleAspectFit
+            animationView.loopMode = .loop
+            animationView.play()
+            self.scrollView.addSubview(animationView)
+
+        }
+    
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        self.navigationController?.isNavigationBarHidden = true
+
+
+    }
+
+
+
+    func setUpScroll(){
+
+        scrollView.delegate = self
+
+        scrollView.contentSize = CGSize(width: view.frame.size.width * 5, height: scrollView.frame.size.height)
+
+        for i in 0...4{
+
+            let onboardLabel = UILabel(frame: CGRect(x: CGFloat(i) * view.frame.size.width, y: view.frame.size.height/3, width: scrollView.frame.size.width, height: scrollView.frame.size.height))
+
+            onboardLabel.font = UIFont.boldSystemFont(ofSize: 15.0)
+            onboardLabel.textAlignment = .center
+            onboardLabel.text = onboardStringArray[i]
+            scrollView.addSubview(onboardLabel)
+        }
+
+
+    }
+    
+    
+
+
+}
+
